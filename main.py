5321753e59eb92d6dad727e8290164e872439a36
Collin Gress <gress126@gmail.com>
2018-09-06 12:56:46 -0500

    working implementation

---
+++ main.py
@@ -0,0 +1,6 @@
+import mcts
+import same_game_env
+
+env = same_game_env.Env()
+mcts = mcts.MCTS(env)
+print(mcts.search(50))
--- mcts.py
+++ mcts.py
@@ -1,3 +1,4 @@
+import copy
 import math
 import random
 
@@ -6,6 +7,7 @@ class Node():
         self._parent = None
         self._children = list()
         self._action = action
+        self._terminal = False
         self._env = env
         self._q = 0
         self._n = 0
@@ -31,39 +33,54 @@ class Node():
         self._n = value
 
     @property
+    def parent(self):
+        return self._parent
+
+    @property
     def children(self):
         return self._children
 
     @property
-    def reward(self)
+    def reward(self):
         return self._env.total_reward
+    
+    @property
+    def terminal(self):
+        return self._terminal
 
     def expand(self):
-        if self._has_children():
+        if self.has_children():
             print('Tried to expand a node which was already expanded')
             return False
         actions = list(self._env.get_possible_moves().keys())
+        if len(actions) == 0:
+            self._terminal = True
+            return False
         for action in actions:
             env = copy.deepcopy(self._env)
             env.step(action)
-            self._children.append(env)
-        return len(action) > 0
+            child = Node(action, env)
+            child._parent = self 
+            self._children.append(child)
+        return True
 
     def has_children(self):
         return len(self._children)
 
     def reset(self):
         self._children.clear()
+        self._terminal = False
         self._q = 0
         self._n = 0
 
 class MCTS():
-    def __init__(self, env, priors=None):
+    def __init__(self, env, random_seed=None, priors=None):
         if random_seed is not None:
             random.seed(random_seed)
         else:
             random.seed()
         self._root = Node(None, env) 
+        self._root.n = 1
         self._curr = self._root
         self._ranked_list = None
 
@@ -74,31 +91,49 @@ class MCTS():
     def tree_policy(self, node): 
         choice = node
         while choice.has_children():
-            choice = best_child(choice)
+            choice = self.best_child(choice)
+        if choice.expand():
+            return self.best_child(choice)
         return choice
 
     def default_policy(self, node):
         cur = node
-        while cur.expand():
+        while not cur.terminal and cur.expand():
             cur = random.choice(cur.children)
-        cur.children.clear()
-        return cur.reward
+        reward = cur.reward
+        print('found playout with reward: ' + str(reward))
+        node.children.clear()
+        return reward
 
     def best_child(self, node): 
         N = node.n
         children = node.children
         max_score = float('-inf') 
         best = list()
+        avg_q = sum(child.q for child in children) / len(children)
         for child in children:
             n = child.n
             q = child.q
-            UCB1 = q/n + math.sqrt(2) * math.sqrt(math.log(N) / n) 
+            if n == 0:
+                UCB1 = float('inf')
+            else:
+                UCB1 = q/n + avg_q * math.sqrt(math.log(N) / n) 
             if UCB1 > max_score:
                 max_score = UCB1
                 best = [child]
             elif UCB1 == max_score:
                 best.append(child)
         return random.choice(best)
+
+    def search(self, iterations):
+        while not self._curr.terminal:
+            self._curr._env.render()
+            for _ in range(iterations):
+                leaf = self.tree_policy(self._curr)
+                reward = self.default_policy(leaf)
+                self.backprop(leaf, reward)
+            self._curr = self.best_child(self._curr)
+        return self._curr.reward
 
     def backprop(self, node, q):
         while node.parent is not None:
