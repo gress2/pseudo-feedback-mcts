d8957d505feb4a146e1b76cd6484846dac2d399e
Collin Gress <gress2@illinois.edu>
2018-10-17 17:29:45 -0500

    add modelling for number of children

--- nc.py
+++ nc.py
@@ -2,30 +2,32 @@ import pymc3 as pm
 import numpy as np
 
 '''
-nc_i ~ Poisson(theta)
-theta = exp(beta * d_i + alpha)
+nc_i ~ floor(N(mu, sigma))
+mu = alpha * depth_i + beta
+sigma ~ HN(10)
 alpha ~ N(0, 10)
-beta ~ N(0, 10)
+beta ~ N(0, 50)
 '''
 
 with open('nc', 'r') as nc_f:
     lines = [line[:-1] for line in nc_f.readlines()]
-
-Y = np.array([int(line.split(',')[0]) for line in lines])
-X = np.array([int(line.split(',')[1]) for line in lines])
+    Y = np.array([int(line.split(',')[0]) for line in lines])
+    X = np.array([int(line.split(',')[1]) for line in lines])
 
 basic_model = pm.Model()
 
 with basic_model:
-    beta = pm.Normal(mu=0, sd=10, name='beta')
     alpha = pm.Normal(mu=0, sd=10, name='alpha')
-    theta = beta * X + alpha
-    like = pm.Poisson(mu=np.exp(theta), observed=Y, name='like')
+    beta = pm.Normal(mu=0, sd=50, name='beta')
+    mu = alpha * X + beta
+    sigma = pm.HalfNormal(sd=10, name='sigma')
+    Y_obs = pm.math.floor(pm.Normal(mu=mu, sd=sigma, observed=Y, name='Y_obs'))
 
 map_estimate = pm.find_MAP(model=basic_model)
 print(map_estimate)
 
 '''
-beta = -0.066
-alpha = 3.567
+alpha <- -0.51
+beta <- 22.79
+sigma <- 1.95
 '''
